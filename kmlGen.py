af02ca5d6961b85590529c1d751bdddec5a66ca4
Regis <omilale2@gmail.com>
2022-03-08 10:59:05 +0100

    v 1.0

--- kmlGen.py
+++ kmlGen.py
@@ -1,18 +1,11 @@
-"""
-Generates KML files
-by intitiating a KML class
-and calling Add point
-options to add polygon by introducing coordinates in function
-requires also context opener
-"""
-class kmlGen:
+
+class kmlGen2:
     def __init__(self,file_name=None):
-        self.listPoints=[]
         if file_name is None:
             self.file_name='test'
         else:self.file_name=file_name
-
-        self.intialXml="""<?xml version="1.0" encoding="UTF-8"?>
+        self.baseFile={'kml':[]}
+        self.initial="""<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
 <Document>
 	<name>test.kml</name>
@@ -43,26 +36,60 @@ class kmlGen:
 			</Icon>
 			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
 		</IconStyle>
-	</Style>
-		"""
-        self.endXml="""</Document></kml>"""
-        self.final=''
-    def constructor(self):
-        pointsa=''.join([a for a in self.listPoints])
-        self.final=self.intialXml+pointsa+self.endXml
+	</Style>"""
+        self.endXml = """</Document></kml>"""
+        self.finale=...
+        self.folderPoints=''
+    def mergeInit(self,name):
+        # < Folder id = {id} >< name > {name} < / name >
+        return f'<Folder><name>{name}</name>'
+    def mergeEnd(self):
+        return '</Folder>'
 
-    def addPoint(self,lat,long):
-        """
-        Adds new placemarks on the kml file
-        :param lat:
-        :param long:
-        :return:
-        """
-        self.listPoints.append(f"<Placemark><name>test</name><styleUrl>#m_ylw-pushpin</styleUrl><Point><gx:drawOrder>1</gx:drawOrder><coordinates>{lat},{long},0</coordinates></Point></Placemark>")
+    def looper(self,dir):
+        #idkeys=1
+        pointsa = ''
+        for key in dir:
+            if isinstance(dir, dict):
+                if len(dir[key]) > 0:
+                    #print(f'added init{key}')
+                    self.folderPoints+=self.mergeInit(key)
+                    #idkeys+=1
+                    pointsa = ''
+                    for a in dir[key]:
+                        if isinstance(a, dict):
+                            self.looper(a)
+                        elif isinstance(a, str):
+                            pointsa += str(a)
+                    #print(pointsa)
+                    self.folderPoints+=pointsa
+                    self.folderPoints +=self.mergeEnd()
+                    #print(f'end{key}')
+                else:
+                    print(type(key))
+            else:
+                break
+    def convert(self):
+        ...
+
+    def addPoint(self,lat,long,root,name):
+        
+        root.append(f"<Placemark><name>{name}</name><styleUrl>#m_ylw-pushpin</styleUrl><Point><gx:drawOrder>1</gx:drawOrder><coordinates>{lat},{long},0</coordinates></Point></Placemark>")
+    def addShape(self,arr,root,name):
+        ...
+
+    def newfolder(self,leaf=None, root=None):
+        if root is None:
+            self.baseFile['kml'].append({leaf: []})
+            return self.baseFile['kml'][-1][leaf]
+        root.append({leaf: []})
+        return root[-1][leaf]
+
+    def constructor(self):
+        self.looper(self.baseFile)
+        self.final=self.initial+self.folderPoints+self.endXml
 
     def save(self):
         self.constructor()
         with open(self.file_name+'.kml', 'w') as myFile:
             myFile.write(self.final)
-
-
